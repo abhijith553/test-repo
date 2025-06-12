dde56e1c1db8c2b9030ab32829dfab296aa9100c
= <=>
2025-06-12 18:45:10 +0530

    added 3 files

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ app.layout = html.H1("Hello from Dash on Render!")
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     print("Starting on port", port)
-    app.run_server(debug=False, host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=port)
