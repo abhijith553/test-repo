7f8837a00c05bdcd2008d1ce45aa65be15c83729
= <=>
2025-06-12 18:36:05 +0530

    added 3 files

---
+++ app.py
@@ -0,0 +1,10 @@
+from dash import Dash, html
+import os
+
+app = Dash(__name__)
+app.layout = html.H1("Hello from Dash on Render!")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    print("Starting on port", port)
+    app.run_server(debug=False, host="0.0.0.0", port=port)
